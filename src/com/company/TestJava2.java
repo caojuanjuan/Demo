2cd0e862369afb2f98b9942e4a739e332351523b
caojj <875936906@qq.com>
2022-04-27 23:37:32 +0800

    second222

---
+++ src/com/company/TestJava2.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class TestJava2 {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("TestJava-------------------");
+    }
+}
